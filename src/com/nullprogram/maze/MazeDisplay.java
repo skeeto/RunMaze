81b8cbd413a38b7e4487fe73cf6ab72dae842289
Christopher Wellons <mosquitopsu@gmail.com>
2011-07-19 10:51:27 -0400

    Make Checkstyle happy with MazeDisplay.java.

--- src/com/nullprogram/maze/MazeDisplay.java
+++ src/com/nullprogram/maze/MazeDisplay.java
@@ -6,27 +6,39 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-/* Contains the actual maze drawing */
+/**
+ * Displays a maze to the user as a GUI component.
+ */
 class MazeDisplay extends JPanel {
     private static final long serialVersionUID = 1L;
 
+    /* Color scheme. */
+    private static final Color SOLUTION = Color.GREEN;
+    private static final Color ERROR = new Color(255, 127, 127);
+    private static final Color WALL = Color.BLACK;
+
     private Maze maze;
-    private int unitSize;
+    private int scale;
 
     private int width;
     private int height;
 
-    public MazeDisplay(Maze view, int unitSize) {
+    /**
+     * Display the given maze at the given size.
+     * @param view  the maze to be displayed
+     * @param unitSize  the pixel size of each cell in the maze
+     */
+    public MazeDisplay(final Maze view, final int unitSize) {
         super();
-        this.unitSize = unitSize;
+        scale = unitSize;
         setMaze(view);
     }
 
-    /* Draws the maze */
-    public void paintComponent(Graphics g) {
+    @Override
+    public void paintComponent(final Graphics g) {
         super.paintComponent(g);
-        g.drawLine(0,height,width,height);
-        g.drawLine(width,0,width,height);
+        g.drawLine(0, height, width, height);
+        g.drawLine(width, 0, width, height);
         for (int i = 0; i < maze.getWidth(); i++) {
             for (int j = 0; j < maze.getHeight(); j++) {
                 drawCell(new OrderedPair(i, j), g);
@@ -34,49 +46,55 @@ class MazeDisplay extends JPanel {
         }
     }
 
-    /* Fills a cell with the current color */
-    private void fillCell(OrderedPair point, Graphics g) {
-        g.fillRect(point.x * unitSize,
-                   point.y * unitSize,
-                   unitSize,
-                   unitSize);
+    /**
+     * Fills the given position with the current color.
+     * @param point the position to be filled
+     * @param g the Graphics object to be painted on
+     */
+    private void fillCell(final OrderedPair point, final Graphics g) {
+        g.fillRect(point.x * scale, point.y * scale,
+                   scale, scale);
     }
 
-    /* Draws the cell according to its state information */
-    private void drawCell(OrderedPair point, Graphics g) {
+    /**
+     * Draw a single cell to the display.
+     * @param point the position of the cell to be drawn
+     * @param g the Graphics to be painted to
+     */
+    private void drawCell(final OrderedPair point, final Graphics g) {
         if (maze.isSolution(point)) {
-            g.setColor(new Color(0, 255, 0));
+            g.setColor(SOLUTION);
             fillCell(point, g);
         }
         if (maze.isError(point)) {
-            g.setColor(new Color(255, 127, 127));
+            g.setColor(ERROR);
             fillCell(point, g);
         }
 
         int x = point.x;
         int y = point.y;
-        g.setColor(new Color(0, 0, 0));
+        g.setColor(WALL);
         if (maze.leftWall(point)) {
-            g.drawLine(x*unitSize,
-                       y*unitSize,
-                       x*unitSize,
-                       (y+1)*unitSize);
+            g.drawLine(x * scale, y * scale,
+                       x * scale, (y + 1) * scale);
         }
         if (maze.topWall(point)) {
-            g.drawLine(x*unitSize,
-                       y*unitSize,
-                       (x+1)*unitSize,
-                       y*unitSize);
+            g.drawLine(x * scale, y * scale,
+                       (x + 1) * scale, y * scale);
         }
     }
 
-    /* Assign this display a new maze. */
-    public void setMaze(Maze view) {
+    /**
+     * Assign a new maze to this display.
+     * @param view the new maze to be displayed
+     */
+    public void setMaze(final Maze view) {
         maze = view;
-        width = maze.getWidth() * unitSize;
-        height = maze.getHeight() * unitSize;
+        width = maze.getWidth() * scale;
+        height = maze.getHeight() * scale;
         Dimension size = new Dimension(width, height);
         setMinimumSize(size);
         setPreferredSize(size);
+        repaint();
     }
 }
